ed456057a3fb0fa1cb13ec66e7b1e23989a8b119
James <james@jamesnzl.xyz>
2022-06-24 16:54:09 +1200

    refactor: :pencil2: rename authorize -> authorise

--- pages/api/authorise.ts
+++ pages/api/authorise.ts
@@ -4,11 +4,11 @@ if (!process.env.PROXY_REDIRECT_URL) {
   throw new Error('PROXY_REDIRECT_URL env variable is not set');
 }
 
-if (!process.env.NOTION_AUTHORIZE_URL) {
-  throw new Error('NOTION_AUTHORIZE_URL env variable is not set');
+if (!process.env.NOTION_AUTHORISE_URL) {
+  throw new Error('NOTION_AUTHORISE_URL env variable is not set');
 }
 
-export default function authorize(req: NextApiRequest, res: NextApiResponse) {
+export default function authorise(req: NextApiRequest, res: NextApiResponse) {
   const { query } = req;
 
   const state = query.state;
@@ -17,7 +17,7 @@ export default function authorize(req: NextApiRequest, res: NextApiResponse) {
   const responseType = query.response_type;
   const selfRedirect = process.env.PROXY_REDIRECT_URL;
 
-  const url = `${process.env.NOTION_AUTHORIZE_URL}?owner=${owner}&client_id=${clientId}&redirect_uri=${selfRedirect}&response_type=${responseType}&state=${state}`;
+  const url = `${process.env.NOTION_AUTHORISE_URL}?owner=${owner}&client_id=${clientId}&redirect_uri=${selfRedirect}&response_type=${responseType}&state=${state}`;
 
   res.redirect(url);
 }
